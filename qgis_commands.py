e7c07fa8c66e950d2923a2e1a839d7090f6802e2
Nathan Woodrow <nathan.woodrow@mapsolutions.com.au>
2015-04-14 23:32:26 +1000

    Add plugin-install, plugin-uninstall commands

--- qgis_commands.py
+++ qgis_commands.py
@@ -4,6 +4,7 @@ import glob
 from qgis.core import *
 from qgis.gui import *
 from qgis.utils import iface
+from pyplugin_installer import installer as plugin_installer
 
 from PyQt4.QtGui import QDockWidget
 
@@ -13,6 +14,52 @@ def layer_by_name(layername):
     return QgsMapLayerRegistry.instance().mapLayersByName(layername)[0]
 
 project_paths = []
+plugins_fetched = False
+
+def fetch_plugins():
+    installer = plugin_installer.pluginInstaller
+    if not plugins_fetched:
+        installer.fetchAvailablePlugins(True)
+        global plugins_fetched
+        plugins_fetched = True
+
+
+def plugins_for_install(argdata, userdata):
+    fetch_plugins()
+    return [plugin['name'] for plugin in plugin_installer.plugins.all().values()] 
+
+
+def installed_plugins(argdata, userdata):
+    fetch_plugins()
+    status = ['installed', 'upgradeable']
+    return [plugin['name'] for plugin in plugin_installer.plugins.all().values() if plugin['status'] in status] 
+
+
+def is_plugin(name):
+    # Fetch plugins only runs once per session
+    fetch_plugins()
+    installer = plugin_installer.pluginInstaller
+    try:
+        plugin_installer.plugins.all()[name]
+        return True, ""
+    except KeyError:
+        return False, "Plugin not found in installer"
+
+
+@command.command("Plugin name")
+@command.check(name=is_plugin)
+@command.complete_with(name=plugins_for_install)
+def plugin_install(name):
+    installer = plugin_installer.pluginInstaller
+    installer.installPlugin(name)
+
+
+@command.command("Plugin name")
+@command.check(name=is_plugin)
+@command.complete_with(name=installed_plugins)
+def plugin_uninstall(name):
+    installer = plugin_installer.pluginInstaller
+    installer.uninstallPlugin(name, quiet=True)
 
 
 @command.command("What is the x?", "What is the y?")
@@ -33,7 +80,7 @@ def point_at(x, y):
 def define_project_paths(paths):
     global project_paths
     project_paths = paths.split(',')
-
+ 
 
 def complete_projects(argname, data):
     # TODO Add auto complete for paths
